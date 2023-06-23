4e72e3aca6ecd7e6b99573252f96a819278c6922
ranggarh <ranggarhoke@gmail.com>
2023-06-23 19:47:15 +0700

    Implementasi Auth pada Project Employee List

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\auth\LoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->middleware('auth');
+
+Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+//Route pertanyaan no 1
+Route::resource('employees', EmployeeController::class)->middleware('auth');
+Route::get('profile', ProfileController::class)->name('profile')->middleware('auth');
+
+Auth::routes();
+
+Route::post('/login', [LoginController::class, 'authenticate']);
